93b9058a303f858e2a0ea046df6225901395c148
Mauro Marzocca <57107500+mauromarzocca@users.noreply.github.com>
2023-05-10 12:24:27 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@
 //  Created by Mauro Marzocca on 09/05/23.
 //
 
-
 #include <stdlib.h>
 #include <iostream>
 #include <string.h>
@@ -17,26 +16,26 @@ using namespace std;
 
 void cesare()
 {
-     const string alfabeto = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
-     const string cifrato = "defghijklmnopqrstuvxyz";
-     
-     string frase("");
-     string risultato ("");
-     getline (cin,frase);
-    
-     cout << "\tInserire la parola da criptare : " ;
-     cin >> frase;
+    const string alfabeto = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
+    const string cifrato = "defghijklmnopqrstuvxyz";
+
+    string frase("");
+    string risultato("");
+    getline(cin, frase);
+
+    cout << "\tInserire la parola da criptare : ";
+    cin >> frase;
 
     for (int i = 0; i < frase.length(); i++)
     {
         bool trovato = false;
         int j = 0;
         for (j = 0; j < alfabeto.length(); j++)
-        if (toupper(frase[i]) == alfabeto[j])
-        {
-           risultato = risultato + cifrato[j];
-           trovato = true;
-        }
+            if (toupper(frase[i]) == alfabeto[j])
+            {
+                risultato = risultato + cifrato[j];
+                trovato = true;
+            }
         if (trovato == false)
         {
             risultato = risultato + frase[i];
@@ -47,37 +46,34 @@ void cesare()
 
 void rot13()
 {
-     
-     string frase("");
-     string risultato("");
-     getline(cin,frase);
-     
-     cout << "\tInserire la parola da criptare : ";
-     cin >> frase;
+
+    string frase("");
+    string risultato("");
+    getline(cin, frase);
+
+    cout << "\tInserire la parola da criptare : ";
+    cin >> frase;
 
     for (int i = 0; i < frase.length(); i++)
     {
         int c = (int)frase[i];
         if (c >= 65 && c <= 77)
-         c = c + 13;
-        else
-         if (c >= 78 && c <= 90)
-          c = c - 13;
-         else
-          if (c >= 97 && c <= 109)
-           c = c + 13;
-          else
-           if (c >= 110 && c <=122)
+            c = c + 13;
+        else if (c >= 78 && c <= 90)
             c = c - 13;
-      risultato = risultato + (char)c;
-      }
-  cout << "\tLa frase criptata utilizzando ROT 13 e': " << risultato << endl;
+        else if (c >= 97 && c <= 109)
+            c = c + 13;
+        else if (c >= 110 && c <= 122)
+            c = c - 13;
+        risultato = risultato + (char)c;
+    }
+    cout << "\tLa frase criptata utilizzando ROT 13 e': " << risultato << endl;
 }
 
 void cif()
 {
-    char t_originale[CARATTERI+1];
-    char t_cifrato[CARATTERI+1];
+    char t_originale[CARATTERI + 1];
+    char t_cifrato[CARATTERI + 1];
     int chiave;
     int lung_string;
     int buffer;
@@ -89,39 +85,41 @@ void cif()
     cin >> chiave;
     if (chiave >= 0)
     {
-    for (int t = 0; t < lung_string; t++)
-    {
-       if (t_originale[t] > 95)
-       {
-          buffer = ((t_originale[t] + chiave) %96) %26;
-          if (buffer == 0)
-          {
-             buffer = 26;
-          }
-          t_cifrato[t] = 96 + buffer;
-       }
-       else
-       {
-          buffer = ((t_originale[t] + chiave) %64) %26;
-          if (buffer == 0)
-          {
-             buffer = 26;
-          }
-          t_cifrato[t]= 64 + buffer;
-       }
+        for (int t = 0; t < lung_string; t++)
+        {
+            if (t_originale[t] > 95)
+            {
+                buffer = ((t_originale[t] + chiave) % 96) % 26;
+                if (buffer == 0)
+                {
+                    buffer = 26;
+                }
+                t_cifrato[t] = 96 + buffer;
+            }
+            else
+            {
+                buffer = ((t_originale[t] + chiave) % 64) % 26;
+                if (buffer == 0)
+                {
+                    buffer = 26;
+                }
+                t_cifrato[t] = 64 + buffer;
+            }
+        }
+        t_cifrato[lung_string] = '\0';
+        cout << "\tTesto cifrato : " << t_cifrato << endl;
     }
-    t_cifrato[lung_string] = '\0';
-    cout << "\tTesto cifrato : " << t_cifrato << endl;
-} else {
-do
-{
-    system("CLS");
-    cout << "\tErrore Inserimento! Valore non valido!\n";
-    cout << "\tParola Inserita : " << t_originale << endl;
-    cout << "\tInserisci la chiave: ";
-    cin >> chiave;
-} while (chiave < 0);
-}
+    else
+    {
+        do
+        {
+            system("CLS");
+            cout << "\tErrore Inserimento! Valore non valido!\n";
+            cout << "\tParola Inserita : " << t_originale << endl;
+            cout << "\tInserisci la chiave: ";
+            cin >> chiave;
+        } while (chiave < 0);
+    }
 }
 
 void decodifica_cesare()
@@ -141,12 +139,12 @@ void decodifica_cesare()
         bool trovato = false;
         int j = 0;
         for (j = 0; j < cifrato.length(); j++)
-        if (toupper(frase[i]) == cifrato[j])
-        {
-           risultato = risultato + alfabeto[j];
-           trovato = true; 
-        }
-        if (trovato == false) 
+            if (toupper(frase[i]) == cifrato[j])
+            {
+                risultato = risultato + alfabeto[j];
+                trovato = true;
+            }
+        if (trovato == false)
         {
             risultato = risultato + frase[i];
         }
@@ -156,86 +154,101 @@ void decodifica_cesare()
 
 void mod()
 {
-     cout << "\t - Versione 1.0" << endl;
-     cout << "\t    Release iniziale. " << endl;
-     cout << "\t - Versione 1.0.1" << endl;
-     cout << "\t    Risoluzione di alcuni Bug. " << endl;
-     cout << "\t - Versione 1.1" << endl;
-     cout << "\t      Codice Migliorato" << endl;
-     cout << "\t - Versione 1.2" << endl;
-     cout << "\t      Release Stabile" << endl;
-     cout << "\t - Versione 1.2.1" << endl;
-     cout << "\t      Bug Fix" << endl;
-     cout << "\t - Versione 1.3" << endl;
-     cout << "\t      Nuova Release" << endl;
-     cout << "\t - Versione 1.4" << endl;
-     cout << "\t      Bug Fix e Nuova Build" << endl;
+    cout << "\t - Versione 1.0" << endl;
+    cout << "\t    Release iniziale. " << endl;
+    cout << "\t - Versione 1.0.1" << endl;
+    cout << "\t    Risoluzione di alcuni Bug. " << endl;
+    cout << "\t - Versione 1.1" << endl;
+    cout << "\t      Codice Migliorato" << endl;
+    cout << "\t - Versione 1.2" << endl;
+    cout << "\t      Release Stabile" << endl;
+    cout << "\t - Versione 1.2.1" << endl;
+    cout << "\t      Bug Fix" << endl;
+    cout << "\t - Versione 1.3" << endl;
+    cout << "\t      Nuova Release" << endl;
+    cout << "\t - Versione 1.4" << endl;
+    cout << "\t      Bug Fix e Nuova Build" << endl;
 }
-
-
 
 int main()
 {
     int scelta = -1;
     do
-{
-      system("cls");
-      cout << endl << "\t-------CIFRATURA------" << endl << endl;
-      cout << "\tPercorso realizzato da Mauro Marzocca" << endl;
-      cout << "\tVersione 1.4" << endl;
-      cout << endl;
-      cout << "\t1)  Cesare" << endl;
-      cout << "\t2)  ROT13" << endl;
-      cout << "\t3)  Cifratura MonoAlfabetica" <<endl << endl;
-      cout << "\t4)  Modifiche versioni Precedenti" << endl;
-      cout << "\t0)  Fine";
-      cout << endl << endl << "\tOpzione: ";
-      cin >> scelta;
-      cout << endl;
-      switch(scelta)
-      {
-              case 1: cesare();
-                        cout << endl << "\t";
-                      system("pause");
-                      scelta = -1;
-                      break;
-                      
-              case 2: rot13();
-                        cout << endl << "\t";
-                      system("pause");
-                      scelta = -1;
-                      break;
-              
-              case 3: cif();
-                        cout << endl << "\t";
-                      system("pause");
-                      scelta = -1;
-                      break;
-                      
-              case 4: mod();
-                        cout << endl << "\t";
-                      system("pause");
-                      
-                      scelta = -1;
-                      break;
+    {
+        system("cls");
+        cout << endl
+             << "\t-------CIFRATURA------" << endl
+             << endl;
+        cout << "\tPercorso realizzato da Mauro Marzocca" << endl;
+        cout << "\tVersione 1.4" << endl;
+        cout << endl;
+        cout << "\t1)  Cesare" << endl;
+        cout << "\t2)  ROT13" << endl;
+        cout << "\t3)  Cifratura MonoAlfabetica" << endl
+             << endl;
+        cout << "\t4)  Modifiche versioni Precedenti" << endl;
+        cout << "\t0)  Fine";
+        cout << endl
+             << endl
+             << "\tOpzione: ";
+        cin >> scelta;
+        cout << endl;
+        switch (scelta)
+        {
+        case 1:
+            cesare();
+            cout << endl
+                 << "\t";
+            system("pause");
+            scelta = -1;
+            break;
 
-                       case 5: decodifica_cesare();
-                        cout << endl << "\t";
-                      system("pause");
-                      scelta = -1;
-                      break;
-              
-              case 0: cout << endl <<"\tGrazie di aver usato il mio Programma!" << endl << endl;
-                        cout << "\t";
-                      system("pause");
-                      return 0;
-                      break;
-                      
-                  default:
-                  cout << "\tErrore Inserimento! Valore non valido!\n\t";
-                  system("pause");
-                  break;
+        case 2:
+            rot13();
+            cout << endl
+                 << "\t";
+            system("pause");
+            scelta = -1;
+            break;
 
-                      }
-} while ((scelta >= 0) || (scelta <= 4));
+        case 3:
+            cif();
+            cout << endl
+                 << "\t";
+            system("pause");
+            scelta = -1;
+            break;
+
+        case 4:
+            mod();
+            cout << endl
+                 << "\t";
+            system("pause");
+
+            scelta = -1;
+            break;
+
+        case 5:
+            decodifica_cesare();
+            cout << endl
+                 << "\t";
+            system("pause");
+            scelta = -1;
+            break;
+
+        case 0:
+            cout << endl
+                 << "\tGrazie di aver usato il mio Programma!" << endl
+                 << endl;
+            cout << "\t";
+            system("pause");
+            return 0;
+            break;
+
+        default:
+            cout << "\tErrore Inserimento! Valore non valido!\n\t";
+            system("pause");
+            break;
+        }
+    } while ((scelta >= 0) || (scelta <= 4));
 }
